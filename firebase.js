4cbac587a929a692e83de065b83ccafeabf94c06
Rilandev <95538487+Rilandev@users.noreply.github.com>
2021-12-04 22:19:21 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAezRLTMHqQRQUJRPK35gl5kmC2c5m7g1g",
+    authDomain: "amzon-2-yt.firebaseapp.com",
+    projectId: "amzon-2-yt",
+    storageBucket: "amzon-2-yt.appspot.com",
+    messagingSenderId: "724560234096",
+    appId: "1:724560234096:web:2486acd903e996268856b5"
+  };
